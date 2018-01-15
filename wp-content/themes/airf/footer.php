ba46b7397859112c6554f3777b7069709429cc05
Garrett Cullen <Garth619@users.noreply.github.com>
2018-01-14 20:08:27 -0800

    push

--- wp-content/themes/airf/footer.php
+++ wp-content/themes/airf/footer.php
@@ -1,4 +1,10 @@
-
+<footer>
+	
+	<span class="copyright">Copyright 2018 Alliance International Rescue Foundation</span><!-- copyright -->
+	
+	<a class="site_by" href="">Site by <img src="<?php bloginfo('template_directory');?>/images/rcm.png"/> </a>
+	
+</footer>
 
 <?php wp_footer();?>
 
