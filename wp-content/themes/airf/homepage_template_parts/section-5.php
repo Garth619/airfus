cb433020fa3dfccbb17ba9862c8a9ce9d179b7de
Garrett Cullen <Garth619@users.noreply.github.com>
2018-01-14 19:12:17 -0800

    push

---
+++ wp-content/themes/airf/homepage_template_parts/section-5.php
@@ -0,0 +1,20 @@
+<section id="section_five">
+	
+	<div class="sec_five_inner">
+		
+		<div class="sec_five_content">
+			
+			<span class="sec_five_header">Make a<br/> Donation</span><!-- sec_five_header -->
+			
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum</p> 
+			
+			<a class="donate_button" href="" target="_blank">Donate</a><!-- donate_button -->
+			
+			</div><!-- sec_five_content -->
+			
+			<img class="photo" src="<?php bloginfo('template_directory');?>/images/photo.jpg"/>
+		
+	</div><!-- sec_five_inner -->
+	
+	
+</section><!-- section_five -->
---
+++ wp-content/themes/airf/homepage_template_parts/section-6.php
@@ -0,0 +1,29 @@
+<section id="section_six">
+	
+	<div class="sec_six_inner">
+		
+		<span class="sec_six_title">Contact Us</span><!-- sec_six_title -->
+		
+		<div class="social_media">
+			
+			<a class="" href=""><img src="<?php bloginfo('template_directory');?>/images/fb.svg"/></a>
+			
+			<a class="" href=""><img src="<?php bloginfo('template_directory');?>/images/instagram.svg"/></a>
+			
+			<a class="" href=""><img src="<?php bloginfo('template_directory');?>/images/twitter.svg"/></a>
+			
+			<a class="" href=""><img src="<?php bloginfo('template_directory');?>/images/google-plus.svg"/></a>
+			
+		</div><!-- social_media -->
+		
+		<a class="contact_tel" href="tel:(913) 636-5090">(913) 636-5090</a>
+		
+		<div class="form_wrapper">
+		
+			<?php gravity_form(1, false, false, false, '', true, 12); ?>
+		
+		</div><!-- form_wrapper -->
+		
+	</div><!-- sec_six_inner -->
+	
+</section><!-- section_six -->
