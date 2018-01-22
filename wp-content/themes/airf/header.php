065f1fd1469b591a8e29c48122bc72369a642eb4
Garrett Cullen <Garth619@users.noreply.github.com>
2018-01-21 21:13:51 -0800

    push

--- wp-content/themes/airf/header.php
+++ wp-content/themes/airf/header.php
@@ -29,6 +29,15 @@
 <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 
 <?php wp_head(); ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-112830092-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-112830092-1');
+</script>
 
 </head>
 
